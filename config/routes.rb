c7862f89baf7a1d36509283f48c87152d0bfcd84
Adrien <github@adrien.sent.com>
2021-10-14 10:09:09 +0200

    improv: add root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
 
+  root 'api/pokemons#index'
+
   get '/api/pokemons', to: 'api/pokemons#index'
   get '/api/pokemons/:id', to: 'api/pokemons#show', as: :api_pokemon_show
 end
